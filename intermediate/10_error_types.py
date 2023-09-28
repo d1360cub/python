b1591afd6cef9b3d5c6d7dbd0561649f2927889e
ojosdegatonegro <diegofernandocubides@gmail.com>
2023-09-27 21:13:14 -0500

    error types

---
+++ intermediate/10_error_types.py
@@ -0,0 +1,30 @@
+# SyntaxError
+#print 'hola comunidad'
+
+# NameError
+#print(variable)
+
+# IndexError
+my_list = ['python', 'js,', 'java', 'react']
+#print(my_list[5])
+
+# ModuleNOtFoundError
+#import maths
+
+# AttributeError
+import math
+#print(math.PI)
+
+# KeyError
+my_dict = {'name': 'jhon', 'lastname': 'doe', 'age': 29}
+#print(my_dict['lasname'])
+
+# ImportError
+#from math import PI
+
+# ValueError
+#my_int = int('10 años')
+#print(my_int)
+
+# ZeroDivisionError
+#print(4/0)
